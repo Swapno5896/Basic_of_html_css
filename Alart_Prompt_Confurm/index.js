8c620019447b6977f3dea9aa98fd1bebe6eabadf
swaono5896 <swapnom73@gmail.com>
2022-09-22 00:59:51 +0600

    alart prompt and confurm is added

---
+++ Alart_Prompt_Confurm/index.js
@@ -0,0 +1,13 @@
+const showAlart = () =>{
+    alert('You should not do this.')
+}
+
+const ConfurmMe =() =>{
+    const response = confirm('What do you want?')
+    console.log(response);
+}
+
+const AskQues =() =>{
+    const response = prompt('What is your name ?')
+    console.log(response);
+}
