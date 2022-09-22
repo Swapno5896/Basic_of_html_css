dff1939668fb3b596d88edd9bee2c2cc8c744b0e
swaono5896 <swapnom73@gmail.com>
2022-09-22 15:55:22 +0600

    local stroage is added

---
+++ localStroage/shopping.js
@@ -0,0 +1,50 @@
+const displayLocalStorageCart = () =>{
+    const cart = getCart()
+    for (const name in cart){
+        displayProduct(name)
+    }
+}
+
+const addItem = () =>{
+    const nameField = document.getElementById('product-name')
+    const name = nameField.value
+    if(!name){
+        return
+    }
+    // Display product
+    displayProduct(name)
+    //  add to local stroage
+    addProductToCart(name)
+    nameField.value = ''
+}
+
+const displayProduct =(name)=>{
+    const ul = document.getElementById('products')
+    const li = document.createElement('li')
+    li.innerText = name
+    ul.appendChild(li)
+}
+
+const getCart = () =>{
+    const cart = localStorage.getItem('cart')
+    let cartObj;
+    if(cart) {
+        cartObj = JSON.parse(cart)
+    }
+    else{
+        cartObj = {}
+    }
+
+    return cartObj;
+}
+
+const addProductToCart = name =>{
+    const cart = getCart();
+    cart[name] = 1
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem('cart',cartStringified)
+    console.log(cart);
+}
+
+
+displayLocalStorageCart()
